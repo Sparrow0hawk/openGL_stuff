03a0121bdc82fd912a6b544756d65d85c0e958d8
sparrow0hawk <alexjcoleman@gmail.com>
2020-09-19 19:15:26 +0100

    creating a purple square

--- exercises/simple_GL.py
+++ exercises/simple_GL.py
@@ -18,12 +18,36 @@ def main():
 
     return None
 
+def square():
+
+    glBegin(GL_QUADS)
+    glVertex2f(100, 100)
+    glVertex2f(200, 100)
+    glVertex2f(200, 200)
+    glVertex2f(100, 200)
+    glEnd()
+
+def iterate():
+    glViewport(0,0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
 def showScreen():
     """ 
     Initial function for creating a window in which graphics can be shown
     """
      # clear screen, returns while screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 0.0, 3.0)
+    square()
+    glutSwapBuffers()
+
 
 if __name__ == '__main__':
     main()
