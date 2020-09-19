b52aeda3dd3a9a203d65b9ca936a644eec8926fe
sparrow0hawk <alexjcoleman@gmail.com>
2020-09-19 19:08:52 +0100

    create a blank openGL screen

--- exercises/simple_GL.py
+++ exercises/simple_GL.py
@@ -1,12 +1,29 @@
 import OpenGL
-import OpenGL.GL
-import OpenGL.GLUT
-import OpenGL.GLU
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 print('Imports successful!')
 
 def main():
 
+    glutInit() # create a glut instance
+    glutInitDisplayMode(GLUT_RGBA) # colour display mode
+    glutInitWindowSize(500, 500) # height and width of window
+    glutInitWindowPosition(0, 0)
+    wind = glutCreateWindow("OpenGL Coding practise")
+    glutDisplayFunc(showScreen)
+    glutIdleFunc(showScreen)
+
+    glutMainLoop()
+
     return None
+
+def showScreen():
+    """ 
+    Initial function for creating a window in which graphics can be shown
+    """
+     # clear screen, returns while screen
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
 if __name__ == '__main__':
     main()
